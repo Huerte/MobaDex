b9fb1878a073b005abc01f99ab55f0a0c31f1474
Huerte <huertejerald@gmail.com>
2025-06-03 10:46:43 +0800

    Added a navbar

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
+from . import utils
 
 def home(request):
+    # heroes = utils.get_all_heroes()
+    # rankings = utils.get_hero_rankings()
+    # return render(request, 'home.html', {'heroes': heroes, 'rankings': rankings, })
     return render(request, 'home.html')
 
 def about(request):
